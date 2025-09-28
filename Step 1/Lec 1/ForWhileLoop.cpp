47bebecfc9b86e02f81e8e33d1459f2b0f309773
Harshit <harshitjain1924@gmail.com>
2025-09-28 21:10:09 +0530

    update

---
+++ Step 1/Lec 1/ForWhileLoop.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    //For Loop
+    int i;
+    for(i=1;i<=5;i++){
+        cout<<"Hello World"<<endl;
+    }
+    cout<<i<<endl;
+
+    //While Loop
+    int j=1;
+    do{
+        cout<<"Do While Test"<<endl;
+        j++;
+    }while(j<=0);
+    cout<<j<<endl;
+}
